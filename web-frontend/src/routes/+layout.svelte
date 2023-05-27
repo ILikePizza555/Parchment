3abaac5dcb2ecb89ea6ae71d625829ac29b93ebc
Izzy Lancaster <avrisaac555@gmail.com>
2023-05-27 13:05:35 -0700

    Using better flex utilities

--- web-frontend/src/routes/+layout.svelte
+++ web-frontend/src/routes/+layout.svelte
@@ -6,8 +6,12 @@
 <div class="sm:container sm:mx-auto">
     <header class="sm:my-4">
         <nav class="navbar sm:rounded-box sm:border border-b shadow-sm bg-base-100">
-            <h1 class="flex-1 text-lg">Parchment</h1>
-            <div class="flex-none">
+            <div class="flex-grow">
+                <a class="btn btn-ghost" href="/">
+                    <h1 class="normal-case text-lg font-semibold">Parchment</h1>
+                </a>
+            </div>
+            <div class="flex-shrink">
                 <UserInfo />
             </div>
         </nav>
