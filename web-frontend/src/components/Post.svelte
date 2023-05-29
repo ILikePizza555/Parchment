f59f2732b29a703eea5db8fa8d6adc627c5c605d
Izzy Lancaster <avrisaac555@gmail.com>
2023-05-28 18:20:26 -0700

    Created post component

---
+++ web-frontend/src/components/Post.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let title: string | undefined;
+    export let content: string;
+    export let created: Date;
+</script>
+
+<div class="my-16">
+    <article class="prose mx-auto border shadow-md p-4 rounded-box">
+        {#if title}
+            <h1>{title}</h1>
+        {/if}
+        {@html content}
+    </article>
+</div>
