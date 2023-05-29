53d4fa48396fbaba1fc99775a2d40a3fcbd7b0ca
Izzy Lancaster <avrisaac555@gmail.com>
2023-05-29 00:00:53 -0700

    Using new types in page.ts

--- web-frontend/src/routes/+page.ts
+++ web-frontend/src/routes/+page.ts
@@ -1,13 +1,21 @@
 import { getList, pb } from "$lib/pocketbase";
-import { Record } from "pocketbase";
+import type { Record } from "pocketbase";
 import type { PageLoad } from './$types';
-import type { PostRecord } from "./types";
+import { PostRecord, type UserRecord } from "$lib/types";
+
+interface PostRecordExpands {
+    original_poster: UserRecord,
+    tags: Record[]
+}
 
 export const load = (async ({fetch}) => {
     const result = await getList(
-        (data) => new Record(data) as PostRecord,
+        (data) => new PostRecord<PostRecordExpands>(data),
         fetch,
-        "/api/collections/posts/records");
+        "/api/collections/posts/records",
+        1,
+        30,
+        { expand: ["original_poster", "tags" ] });
 
     return {
         postsIndex: new Map(result.items.map((v, i) => [v.id, i])),
