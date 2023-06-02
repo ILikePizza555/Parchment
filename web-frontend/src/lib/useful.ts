57d840c7751f485de2f7c6fa47678f05ae5f9414
Izzy Lancaster <avrisaac555@gmail.com>
2023-06-02 16:26:25 -0700

    Extracted normalizeIteratee function

--- web-frontend/src/lib/useful.ts
+++ web-frontend/src/lib/useful.ts
@@ -6,6 +6,17 @@ export type TypedKeyOf<T, K> = {[X in keyof T]-?: T[X] extends K ? X : never}[ke
 export type Iteratee<T, V> = ((obj: T) => V) | TypedKeyOf<T, V>;
 export type ComparatorIteratee<T> = Iteratee<T, -1 | 0 | 1>;
 
+export function normalizeIteratee<T, V>(iteratee: Iteratee<T, V>): (o: T) => V {
+    if (typeof iteratee !== "function") {
+        // Replace iteratee with key function.
+        // Use a cast because the type-checker isn't able to understand that TypedKeyOf only selects keys with type V
+        const key = iteratee;
+        return (o: T) => o[key] as V;
+    }
+
+    return iteratee;
+}
+
 /**
  * Performs a binary search on a sorted array to find the index of where the item should be inserted in the array in order to
  * maintain the sort. 
@@ -14,17 +25,11 @@ export type ComparatorIteratee<T> = Iteratee<T, -1 | 0 | 1>;
  * @param iteratee Either a function that takes an item from the array and returns a comparable value, or a key in the object whose value is used for comparison.
  * @returns A number in the range [0, sortedArray.length].
  */
-export function findInsertIndexBy<T>(sortedArray: ArrayLike<T>, itemToInsert: T, iteratee: ComparatorIteratee<T>): number;
 export function findInsertIndexBy<T, V>(sortedArray: ArrayLike<T>, itemToInsert: T, iteratee: Iteratee<T, V>): number {
-    if (typeof iteratee !== "function") {
-        const key = iteratee;
-        // Replace iteratee with key function.
-        // Use a cast because the type-checker isn't able to understand that TypedKeyOf only selects keys with type V
-        iteratee = (o: T) => o[key] as V;
-    }
+    iteratee = normalizeIteratee(iteratee);
     
     const value = iteratee(itemToInsert);
-    let low = 1, high = sortedArray.length - 1;
+    let low = 0, high = sortedArray.length - 1;
 
     while (low < high) {
         const mid = Math.floor((high - low) / 2),
